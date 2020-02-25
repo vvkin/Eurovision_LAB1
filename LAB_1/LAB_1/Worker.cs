9ee49e58ffd2948bf3380136edff1a92259ad71a
vvkin <vkincur@gmail.com>
2020-02-25 20:49:28 +0200

    created DoWork method

--- LAB_1/LAB_1/Worker.cs
+++ LAB_1/LAB_1/Worker.cs
@@ -8,5 +8,20 @@ namespace LAB_1
     {
         private readonly string dirName;
         public Worker(string dirName) => this.dirName = dirName;
+
+        private Dictionary<string, int> DoWork()
+        {
+            Parser parser = new Parser(dirName);
+            (List<string> countriesList, List<List<int>> gradesList) = parser.GetInput();
+            int[] gradesForPlace = new int[] { 12, 10, 8, 7, 6, 5, 4, 3, 2, 1 };
+            Dictionary<string, int> results = new Dictionary<string, int>();
+            int upBound = (10 < countriesList.Count + 1) ? 10 : countriesList.Count + 1;
+
+            foreach (var countryName in countriesList)
+            {
+                results.Add(countryName, 0);
+            }
+            return results;
+        }
     }
 }
