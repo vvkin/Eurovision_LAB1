fadf7a8759b96da7ef5e070d9f04ac007bc2734d
vvkin <vkincur@gmail.com>
2020-02-25 13:13:13 +0200

    started development GetInput method

--- LAB_1/LAB_1/Parser.cs
+++ LAB_1/LAB_1/Parser.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.IO;
 
 namespace LAB_1
 {
@@ -8,5 +7,24 @@ namespace LAB_1
     {
         private readonly string dirName;
         public Parser(string dirName) => this.dirName = dirName;
+
+
+        public (List<string>, List<List<int>>) GetInput()
+        {
+            string dirPath = "../../../" + dirName;
+            string[] filesList = Directory.GetFiles(dirPath);
+            List<string> countries = new List<string>();
+            List<List<int>> gradesMatrix = new List<List<int>>();
+
+            foreach (var fileName in filesList)
+            {
+                StreamReader cr = new StreamReader(fileName);
+                int countryNum = int.Parse(cr.ReadLine());
+                // Waiting for ParseRow function
+            }
+            return (countries, gradesMatrix);
+        }
+
+
     }
 }
