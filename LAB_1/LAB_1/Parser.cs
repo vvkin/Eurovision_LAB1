019292dd3a4ec6d2c1f22ba02b4fb0763b3b5ab6
vvkin <vkincur@gmail.com>
2020-02-25 13:05:13 +0200

    added Parser class constructor

---
+++ LAB_1/LAB_1/Parser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB_1
+{
+    class Parser
+    {
+        private readonly string dirName;
+        public Parser(string dirName) => this.dirName = dirName;
+    }
+}
