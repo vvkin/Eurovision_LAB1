2420f4000fc2b9c454263e5ae853c2ceaf520afe
DenisioMytnysiano <den.mytnyk@ua.fm>
2020-02-25 19:42:15 +0200

    Worker class added

---
+++ LAB_1/LAB_1/Worker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LAB_1
+{
+    class Worker
+    {
+        private readonly string dirName;
+        public Worker(string dirName) => this.dirName = dirName;
+    }
+}
